5ba127347903db3b587b910d2ba28400a3879803
kartik <kartikb.5578@gmail.com>
2024-01-22 14:17:30 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import {marketplace_abi, nft_abi} from "./abi.js"
+import { ethers } from 'ethers';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Hero from './components/Home.jsx';
+import Create from './components/Create.jsx';
+import MyItem from './components/MyItem.jsx';
+import MyPurchases from "./components/Mypurchases.jsx";
+import Nav from './components/Nav.jsx';
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState("");
+  const [marketplace, setMarketplace]= useState({});
+  const [nft, setNFT] = useState({})
+
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload()
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        setLoading(false)
+        let marketplaceAddress = "0x5B2cEb3b2c0981f91BaDec22302dC064b2D9339c";
+        let nftAddress = "0x57d43b02FDdc168E4684a0F4B7f91BfdEa68b8c4";
+
+        const marketplacecontract = new ethers.Contract(
+          marketplaceAddress,
+          marketplace_abi,
+          signer
+        );
+
+        const nftcontract = new ethers.Contract(
+          nftAddress,
+          nft_abi,
+          signer
+        )
+
+        //console.log(contract);
+        setMarketplace(marketplacecontract);
+        setNFT(nftcontract)
+       
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+
+    provider && loadProvider();
+  }, []);
+
+
   return (
+   
+    <BrowserRouter>
+     <ToastContainer/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav account={account}/>
+      {
+        loading ? (<div>Connecting to Metamask</div>) :(
+          <Routes>
+          <Route path='/' element={<Hero marketplace={marketplace} nft={nft} />}/>
+          <Route path='/create'  element={<Create marketplace={marketplace} nft={nft} />}/>
+          <Route path='/my-listed-nfts' element={<MyItem marketplace={marketplace} nft={nft} account={account} />}/>
+          <Route path='/my-purchases' element={<MyPurchases marketplace={marketplace} nft={nft} account={account} />} />
+        </Routes>
+        )}
+    
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Create.jsx
@@ -0,0 +1,177 @@
+import { useEffect, useState } from 'react'
+import { ethers } from "ethers"
+import { Row, Form, Button } from 'react-bootstrap'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+
+
+const Create = ({ marketplace, nft }) => {
+
+    const [nftimage, setNFTImage] = useState();
+    const [forminfo, setFormInfo] = useState({
+      title:"",
+      description:"",
+      price: null
+    });
+
+    useEffect(()=>{
+      document.title = "Create"
+  }, []);
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormInfo((prevState) => ({ ...prevState, [name]: value }));
+    };
+  
+    const changeHandler = (event) => {
+      setNFTImage(event.target.files[0]);
+    };
+  
+    const handleEvent = async (e) => {
+      e.preventDefault();
+      console.log(nftimage)
+      console.log(forminfo);
+
+      const formData = new FormData();
+      const jsonformData = new FormData();
+  
+      formData.append('file', nftimage);
+  
+      const metadata = JSON.stringify({
+          name: forminfo.title,
+          description: forminfo.description
+      });
+      jsonformData.append('pinataMetadata', metadata);
+      
+      const options = JSON.stringify({
+          cidVersion: 0,
+      })
+      jsonformData.append('pinataOptions', options);
+  
+      try{
+  
+          const resFile = await axios({
+              method: "post",
+              url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+              data: formData,
+              headers: {
+                pinata_api_key: `0a13ef76fb9e01561e05`,
+                pinata_secret_api_key: `f0a2d096004e4f0483a64d06236ddc252b8d8acf612cde6465bc78f013a08ab0`,
+                "Content-Type": "multipart/form-data",
+              },
+            });
+  
+          console.log(resFile.data);
+  
+          const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+  
+          const info ={
+              name: forminfo.title,
+              description: forminfo.description,
+              image: ImgHash,
+              price: forminfo.price
+          }
+  
+          async function pinJSONToPinata(info) {
+              const url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
+              const headers = {
+                  'Content-Type': 'application/json',
+                  'pinata_api_key': `0a13ef76fb9e01561e05`,
+                  'pinata_secret_api_key': `f0a2d096004e4f0483a64d06236ddc252b8d8acf612cde6465bc78f013a08ab0`
+              };
+  
+              try {
+                  const res = await axios.post(url, info, { headers });
+                  const meta = `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`
+                  console.log(meta);
+                  mintThenList(meta);
+              } catch (error) {
+                  console.error(error);
+              }
+  
+          }
+      
+       pinJSONToPinata(info)
+  
+        //   setFormInfo({
+        //       title:"",
+        //       description:"",
+        //       price:""
+
+        //   })
+        //   setNFTImage(null);
+      
+        } catch (error) {
+          console.log(error);
+        }
+  
+  
+      
+    };
+
+
+  const mintThenList = async (uri) => { 
+    toast.info("Confirm to Mint the NFT", {
+      position: "top-center"
+    })
+  const tx1=  await(await nft.mint(uri))
+
+  toast.info("Wait till transaction Confirms....", {
+    position: "top-center"
+  })
+
+  await tx1.wait()
+    const id = await nft.tokenCount()
+
+    toast.info("Approve To sell NFT on marketplace", {position:"top-center"})
+    
+  const tx2 = await(await nft.setApprovalForAll(marketplace.address, true))
+
+  toast.info("Wait till transaction Confirms....", {
+    position: "top-center"
+  })
+
+  await tx2.wait()
+    
+    toast.info("Confirm to Add Item to Marketplace", {position:"top-center"})
+    const listingPrice = ethers.utils.parseEther(forminfo.price.toString())
+   const tx3 =  await(await marketplace.makeItem(nft.address, id, listingPrice))
+   toast.info("Wait till transaction Confirms....", {
+    position: "top-center"
+  })
+
+  await tx3.wait()
+    toast.success("NFT added to marketplace successfully", {position:"top-center"})
+  }
+
+
+  return (
+    <div className="container-fluid mt-5">
+      <div className="row">
+        <main role="main" className="col-lg-12 mx-auto" style={{ maxWidth: '1000px' }}>
+          <div className="content mx-auto">
+            <Row className="g-4">
+              <Form.Control
+                type="file"
+                required
+                name="file"
+                onChange={changeHandler}
+              />
+              <Form.Control onChange={handleChange} name="title" id="title"  size="lg" required type="text" placeholder="Name" />
+              <Form.Control onChange={handleChange} name="description" id="description" size="lg" required as="textarea" placeholder="Description" />
+              <Form.Control onChange={handleChange} name="price" id="price" size="lg" required type="number" placeholder="Price in ETH" />
+              <div className="d-grid px-0">
+                <Button onClick={handleEvent} variant="primary" size="lg">
+                  Create & List NFT!
+                </Button>
+              </div>
+            </Row>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Create
